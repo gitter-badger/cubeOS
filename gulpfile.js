9fa159d7c12efb7b55b84de8185577a675ecd3b6
RocketBear27 <rocketbear27.code@gmail.com>
2018-12-28 20:49:36 -0800

    Setup Gulp Minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-/*
-Resources:
-
-https://github.com/jonschlinkert/gulp-htmlmin
-https://github.com/terinjokes/gulp-uglify
-https://github.com/scniro/gulp-clean-css
-
-*/
-
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+let cleanCSS = require('gulp-clean-css');
 var pipeline = require('readable-stream').pipeline;
-var path = require('path');
+
+gulp.task('html', (cb) => {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('dist'));
+    cb();
+});
+
+
+gulp.task('css', (cb) => {
+    return gulp.src('src/style.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('dist'));
+    cb();
+});
 
