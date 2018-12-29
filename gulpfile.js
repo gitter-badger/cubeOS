1fc6683b6967d3cd6efd221a8875cd66bb6a0d09
RocketBear27 <rocketbear27.code@gmail.com>
2018-12-28 20:18:44 -0800

    Init Gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/*
+Resources:
+
+https://github.com/jonschlinkert/gulp-htmlmin
+https://github.com/terinjokes/gulp-uglify
+https://github.com/scniro/gulp-clean-css
+
+*/
+
+var gulp = require('gulp');
+var pipeline = require('readable-stream').pipeline;
+var path = require('path');
+
